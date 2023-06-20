854e65ee2ba7110a61a261664cd1ff4fb9df1224
Hilbert Muniz de Andrade <hilbertmunizdeandrade@gmail.com>
2023-06-19 21:34:22 -0300

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   // Estado para armazenar a lista de compras
@@ -6,6 +6,12 @@ export default function App() {
 
   // Estado para armazenar o valor do item sendo digitado
   const [item, setItem] = useState("");
+
+  const removerLocalStorage = ()=>{
+    localStorage.removeItem('lista')
+    setListaCompras()
+
+  }
 
   // Função para adicionar um item à lista de compras
   const adicionarItem = () => {
@@ -15,6 +21,43 @@ export default function App() {
       setItem(""); // Limpa o campo de entrada
     }
   };
+
+  //Pratica 1
+
+  const salveLocalStorage = () =>{
+
+    const listaString = JSON.stringify(listaCompras)
+    localStorage.setItem('lista', listaString)
+
+  }
+  //Pratica 2
+
+  const getItemLocalStorage = () => {
+    //Peguei do local storage
+    const listaString = localStorage.getItem('lista',)
+    //Transformei em Array
+   // const listaArray = JSON.parse(listaString)
+
+   // const getLista =  JSON.parse(localStorage.getItem('lista'))
+    
+    //if(listaArray){setListaCompras(listaArray)}
+    listaCompras && setListaCompras(listaCompras)
+    
+  }
+  useEffect(()=>{
+    getItemLocalStorage()
+
+  }, [])
+
+  useEffect(()=>{ 
+
+    if(listaCompras.length){
+
+      salveLocalStorage()}
+
+  }, [listaCompras])
+
+ 
 
   return (
     <div>
@@ -26,6 +69,9 @@ export default function App() {
         placeholder="Digite um item"
       />
       <button onClick={adicionarItem}>Adicionar</button>
+      <button onClick={salveLocalStorage}>Salvar no local</button>
+      <button onClick={getItemLocalStorage}>Pegar do Local Storage</button>
+      <button onClick={removerLocalStorage}>Remover do Local Storage</button>
 
       <ul>
         {listaCompras.map((compra, index) => (
